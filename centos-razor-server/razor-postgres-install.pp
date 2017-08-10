fc78514554d0e11da362892f0591b6db1ef80591
Mikhail Petrov <mikhail.petrov@lazada.com>
2017-08-10 20:15:20 +0300

    Some fixes in razor install.

--- centos-razor-server/razor-postgres-install.pp
+++ centos-razor-server/razor-postgres-install.pp
@@ -1,11 +1,11 @@
 class { 'postgresql::globals':
   manage_package_repo => true,
-  version             => '9.2',
+  version             => '9.6',
 }->
 
 class { 'postgresql::server': }
 
-postgresql::server::db { 'razor':
+postgresql::server::db { 'razor_prd':
   user     => 'razor',
   password => postgresql_password('razor', 'mypass'),
 }
--- centos-razor-server/start.sh
+++ centos-razor-server/start.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-mkernelurl=http://links.puppetlabs.com/razor-microkernel-latest.tar
-mkerneltar=`basename $mkernelurl`
-repodir=/var/lib/razor/repo-store
-
-# Setup the postgres DB if it hasnt been.
-if [[ ! -e /var/lib/pgsql/.bash_profile ]]; then
-     puppet apply /razor-postgres-install.pp
-     cd /opt/razor
-     cp config.yaml.sample config.yaml
-     sed -i 's/razor_prd/razor/' config.yaml
-     jruby bin/razor-admin -e production migrate-database
-     torquebox deploy --env production
-fi
-
-# Install the Razor Microkernel, if it hasnt yet.
-if [[ ! -e $repodir/microkernel/vmlinuz0 ]]; then
-     cd $repodir
-     curl -LO $mkernelurl
-     tar xfva $mkerneltar
-fi
-
-/etc/init.d/postgresql-9.2 start && \
-     torquebox run --bind-address=0.0.0.0
+# mkernelurl=http://links.puppetlabs.com/razor-microkernel-latest.tar
+# mkerneltar=`basename $mkernelurl`
+# repodir=/var/lib/razor/repo-store
+# 
+# # Setup the postgres DB if it hasnt been.
+# if [[ ! -e /var/lib/pgsql/.bash_profile ]]; then
+#      puppet apply /razor-postgres-install.pp
+#      cd /opt/razor
+#      cp config.yaml.sample config.yaml
+#      sed -i 's/razor_prd/razor/' config.yaml
+#      jruby bin/razor-admin -e production migrate-database
+#      torquebox deploy --env production
+# fi
+# 
+# # Install the Razor Microkernel, if it hasnt yet.
+# if [[ ! -e $repodir/microkernel/vmlinuz0 ]]; then
+#      cd $repodir
+#      curl -LO $mkernelurl
+#      tar xfva $mkerneltar
+# fi
+# 
+# /etc/init.d/postgresql-9.6 start && \
+#      torquebox run --bind-address=0.0.0.0
+# 
+# # exec /usr/sbin/init
+/etc/init.d/postgresql-9.6 start
+while ! service postgresql-9.6 status ; do sleep 1; done
+sleep 60
+/usr/sbin/razor-admin -e production migrate-database
+service razor-server start
+while true; do sleep 1000; done
